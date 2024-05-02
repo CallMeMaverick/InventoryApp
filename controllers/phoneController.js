8854843fa88cc9381336e226a2b55c3a089be9ec
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-05-02 16:38:41 +0200

    feat: Page for each phone
    Implemented detailed view for each phone

--- controllers/phoneController.js
+++ controllers/phoneController.js
@@ -24,3 +24,21 @@ exports.phone_list = asyncHandler(async (request, response) => {
     console.log(phones);
     response.render("phone_view", { header: "Phone list", phoneList: phones })
 })
+
+exports.phone_detail = asyncHandler(async (request, response, next) => {
+    const [phone, phoneInstances] = await Promise.all([
+        Phone.findById(request.params.id).populate("company").exec(),
+        PhoneInstances.findById(request.params.id).populate("phone").exec()
+    ]);
+
+    if (!phone) {
+        const err = new Error("Could not fetch the phone");
+        err.status = 404;
+        next(err);
+    } else {
+        response.render("phone_detail", {
+            phone: phone,
+            phoneInstances: PhoneInstances
+        })
+    }
+})
--- routes/catalog.js
+++ routes/catalog.js
@@ -8,4 +8,6 @@ router.get('/', phoneController.index);
 
 router.get('/catalog', phoneController.phone_list);
 
+router.get('/catalog/:id', phoneController.phone_detail);
+
 module.exports = router;
