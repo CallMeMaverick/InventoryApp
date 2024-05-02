329284fa3327c37bdf04f3df851ec5757bd1a5ce
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-05-02 12:44:23 +0200

    feat: Preliminary phone model
    Created a phone model for integrating with db

---
+++ models/phone.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PhoneSchema = new Schema({
+    name: { type: String, required: true },
+    company: { type: Schema.Types.ObjectId, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true }
+})
+
+PhoneSchema.virtual("url").get(function() {
+    return `/home/catalog/${this._id}`
+})
+
+module.exports = mongoose.model("Phone", PhoneSchema);
