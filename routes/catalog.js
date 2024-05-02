c35cf887019b1eef5f9426ac9eee3b57c0547bc3
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-05-02 16:46:33 +0200

    feat: View and controller for companies
    Implemented both controller and view for companies page

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const phoneController = require("../controllers/phoneController");
+const companyController = require("../controllers/companyController")
 
 // BOOK ROUTES
 router.get('/', phoneController.index);
@@ -9,5 +10,7 @@ router.get('/', phoneController.index);
 router.get('/catalog', phoneController.phone_list);
 
 router.get('/catalog/:id', phoneController.phone_detail);
+
+router.get('/brands', companyController.company_list);
 
 module.exports = router;
